a654d08beb9bd80f8657b517bf7a30f5a20d5799
parma1111 <parmarambuldak800@gmail.com>
2022-11-10 11:59:21 +0530

    updated-api

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 export const geoApiOptions = {
     method: "GET",
     headers: {
-        "X-RapidAPI-Key": "bffcde8760msh07a760b0085d80bp1ec447jsn011f9ffbdf48",
+        "X-RapidAPI-Key": "", // enter your rapid api key here
         "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
     },
 };
@@ -9,4 +9,4 @@ export const geoApiOptions = {
 export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
 
 export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
-export const WEATHER_API_KEY = "d9bfaa99927b600a73906e3e2680d885";
+export const WEATHER_API_KEY = ""; // enter your key from openweather API
