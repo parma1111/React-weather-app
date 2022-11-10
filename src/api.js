f1ae6c20ecb21fda0e54d23c05e1c650de3a4082
parma1111 <parmarambuldak800@gmail.com>
2022-11-10 11:38:07 +0530

    react-weather-app

---
+++ src/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+    method: "GET",
+    headers: {
+        "X-RapidAPI-Key": "bffcde8760msh07a760b0085d80bp1ec447jsn011f9ffbdf48",
+        "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+    },
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "d9bfaa99927b600a73906e3e2680d885";
